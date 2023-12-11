02460cdb5bbfb419b44e7cd5ff3c21e60e30e54f
HsiaoCz <984274788@qq.com>
2023-12-12 07:58:28 +0800

    foo

--- etc/etc.go
+++ etc/etc.go
@@ -1,2 +1,9 @@
 package etc
 
+import "github.com/spf13/viper"
+
+func ParseConfig(){
+	viper.SetConfigName("foo")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("../.")
+}
--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
+import (
+	"log"
+
+	"github.com/HsiaoCz/foo/router"
+)
+
 // foo i have no idea what i want to do
 // so i named foo
 // jsut foo
 
-func main(){
+var (
+	addr = "127.0.0.1:3001"
+)
 
+func main() {
+	if err := router.Router(addr); err != nil {
+		log.Fatal(err)
+	}
 }
