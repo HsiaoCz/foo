4d6bcfe2275e4de1a3301d92cb713c1769f49fc7
HsiaoCz <984274788@qq.com>
2023-12-19 07:03:11 +0800

    foo

--- User/cmd/main.go
+++ User/cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main(){
-	
+import (
+	"log"
+
+	"github.com/HsiaoCz/foo/User/server"
+)
+
+var (
+	network = "tcp"
+	addr    = "127.0.0.1:3002"
+)
+
+func main() {
+	if err := server.ResGrpcServer(network, addr); err != nil {
+		log.Fatal(err)
+	}
 }
